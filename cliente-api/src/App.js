777f0b3fcd4ac9af248c70a74ef43b8a1911ad03
KewinLizarraga <kewin392@gmail.com>
2019-12-20 01:27:09 -0500

    [ADD] cliente-API CRM

---
+++ cliente-api/src/App.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react';
+// Routing
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Layout
+import Header from './components/layout/Header';
+import Navegacion from './components/layout/Navegacion';
+// Componentes
+import Clientes from './components/clientes/Clientes';
+import NuevoCliente from './components/clientes/NuevoCliente';
+import EditarCliente from './components/clientes/EditarCliente';
+
+import Productos from './components/productos/Productos';
+import NuevoProducto from './components/productos/NuevoProducto';
+import EditarProducto from './components/productos/EditarPorducto';
+
+import Pedidos from './components/pedidos/Pedidos';
+
+function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Header />
+        <div className="grid contenedor contenido-principal">
+          <Navegacion />
+          <main className="caja-contenido col-9">
+            <Switch>
+              <Route exact path="/" component={Clientes} />
+              <Route exact path="/clientes/nuevo" component={NuevoCliente} />
+              <Route exact path="/clientes/editar/:id" component={EditarCliente} />
+
+              <Route exact path="/productos" component={Productos} />
+              <Route exact path="/productos/nuevo" component={NuevoProducto} />
+              <Route exact path="/productos/editar/:id" component={EditarProducto} />
+
+              <Route exact path="/pedidos" component={Pedidos} />
+            </Switch>
+          </main>
+        </div>
+      </Fragment>
+
+    </Router>
+  );
+}
+
+export default App;
