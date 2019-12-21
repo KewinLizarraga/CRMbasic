d4456a12b2a16a7eca95903589188a4bac517320
KewinLizarraga <kewin392@gmail.com>
2019-12-20 22:04:13 -0500

    [UPDATE] add NuevoPedido

--- cliente-api/src/App.js
+++ cliente-api/src/App.js
@@ -14,6 +14,7 @@ import NuevoProducto from './components/productos/NuevoProducto';
 import EditarProducto from './components/productos/EditarPorducto';
 
 import Pedidos from './components/pedidos/Pedidos';
+import NuevoPedido from './components/pedidos/NuevoPedido';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
               <Route exact path="/productos/editar/:id" component={EditarProducto} />
 
               <Route exact path="/pedidos" component={Pedidos} />
+              <Route exact path="/pedidos/nuevo/:id" component={NuevoPedido} />
             </Switch>
           </main>
         </div>
